b26b22132dcadb895f021487878e701d28e3b6aa
Tiffany Marroquin <tiffany.marroq@gmail.com>
2018-10-12 00:26:30 -0400

    added date function

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -4,7 +4,7 @@
         <p>Hello, I'm a developer from NYC. Graduated from Binghamton University with a 
         degree in Mathematics. Like most graduates, or just me, I did not have a specific path I wanted to do as a career.
         At the end of 2017, I decided to take it upon myself to learn web development/applications. To push myself in the field, I decided to work as a freelance developer.
-        and Here we are!</p>
+        And Here we are!</p>
         <h1>Things I know</h1>
         <ul>
             <li>Node JS</li>
@@ -20,6 +20,7 @@
             <li>Management</li>
             <li>Teamwork</li>
         </ul>
+        <h1>Projects</h1>
         <h1>fun facts</h1>
         <p>Dog lover</p>
         <p>Tea Drinker</p>
@@ -28,10 +29,39 @@
         
         <h1>Want to know more?</h1>
         <p>Ask me! </p>
+
+        <p>Thanks for stopping by!</p>
+        <p>Have a great {{getDay()}}</p>
         <em>social media</em>
         
     </div>
 </template>
+<script>
+export default {
+    data(){
+        return{
+            days: [
+                'Sunday',
+                 'Monday',
+                 'Tuesday',
+                 'Wednesday',
+                 'Thursday',
+                 'Friday',
+                 'Saturday'
+                ]
+                
+            
+        }
+
+    },
+    methods: {
+        getDay(){
+            let today = new Date();
+            return( this.days[today.getDay()]);
+        }
+    }
+}
+</script>
 
 <style scoped>
     .about-page{
